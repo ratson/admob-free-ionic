496e2a991b3c5dfe734d2f9baf0e9fbc2c05edd5
Daniel Rodriguez <dRodriguez@andigital.com>
2017-11-20 16:38:15 +0000

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,36 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private admobFree: AdMobFree) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      this.showBanner();
     });
+  }
+
+  showBanner() {
+    // working examples
+    // banner: 'ca-app-pub-2945024608555072/7944190089'
+    // interstitial: 'ca-app-pub-2945024608555072/7167484407'
+
+    const bannerConfig: AdMobFreeBannerConfig = {
+      // add your config here
+      // for the sake of this example we will just use the test config
+      isTesting: true,
+      autoShow: true
+     };
+     this.admobFree.banner.config(bannerConfig);
+
+     this.admobFree.banner.prepare()
+       .then(() => {
+         // banner Ad is ready
+         // if we set autoShow to false, then we will need to call the show method here
+       })
+       .catch(e => console.log(e));
   }
 }
 
